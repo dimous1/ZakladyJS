c2390036496ea7c629b1a894292df04dca3e6cb5
dimous1 <dmytrotsynyavskyy@seznam.cz>
2024-09-23 12:03:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function name() {
+    let input = prompt("Please enter your name:");
+    console.log("Ahoj, " + input);
+}
+
+
+function age() {
+    if (age % 2 == 0) {
+        console.log("Cislo je sude.")
+    }
+}
+
+age(50);
